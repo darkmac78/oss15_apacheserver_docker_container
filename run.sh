55917da7c31c8dae6abe8ba24214764cad6824d4
darkmac78 <36972321+darkmac78@users.noreply.github.com>
2020-05-10 17:28:58 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 mkdir /tmp/apache_dockerimage
 cd /tmp/apache_dockerimage
 wget https://raw.githubusercontent.com/darkmac78/oss15_apacheserver_docker_container/master/Dockerfile?token=AI2COIMQ5DTPZGF7KY56DQK6XAHKG -O Dockerfile
-clear
 ls /tmp/apache_dockerimage
 sleep 5
 docker build -t=oss152_webserver ./
 docker run -d --name=oss15_WebServer -v /sys/fs/cgroup:/sys/fs/cgroup:ro oss152_webserver
-clear
 docker images
 echo ""
 echo ""
