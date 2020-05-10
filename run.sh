2e4ae292753b19810e3ed014e9219554adef172b
darkmac78 <36972321+darkmac78@users.noreply.github.com>
2020-05-10 17:28:04 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 mkdir /tmp/apache_dockerimage
 cd /tmp/apache_dockerimage
-wget https://raw.githubusercontent.com/darkmac78/oss15_apacheserver_docker_container/master/Dockerfile?token=AI2COIMQ5DTPZGF7KY56DQK6XAHKG
+wget https://raw.githubusercontent.com/darkmac78/oss15_apacheserver_docker_container/master/Dockerfile?token=AI2COIMQ5DTPZGF7KY56DQK6XAHKG -O Dockerfile
 clear
 ls /tmp/apache_dockerimage
 sleep 5
